57b1f3c91b852f3222eddf8b636af32ff1cdd560
hadi2007yahya-sketch <hadi2007yahya@gmail.com>
2026-07-08 17:27:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const express = require('express');
-
-// Botu başlat
+const { Client, GatewayIntentBits, ChannelType } = require('discord.js');
 const client = new Client({ 
     intents: [
         GatewayIntentBits.Guilds, 
-        GatewayIntentBits.GuildMessages, 
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.GuildVoiceStates 
     ] 
 });
+const GUILD_ID = "1446797947399569460";
+const SOURCE_CHANNEL_ID = "1524385072570961920";
 
-// Botun uyumaması için Express (Web sunucusu)
-const app = express();
-app.get('/', (req, res) => res.send('Bot aktif!'));
-app.listen(process.env.PORT || 3000);
+client.on('voiceStateUpdate', async (oldState, newState) => {
+    if (newState.channelId === SOURCE_CHANNEL_ID) {
+        const guild = newState.guild;
+        const newChannel = await guild.channels.create({
+            name: `Oda - ${newState.member.user.username}`,
+            type: ChannelType.GuildVoice,
+            parent: newState.channel.parent,
+        });
+        await newState.member.voice.setChannel(newChannel);
+    }
 
-// Bot hazır olduğunda
-client.once('ready', () => {
-    console.log(`${client.user.tag} olarak giriş yapıldı!`);
-});
-
-// Buraya kendi komutlarını ekleyebilirsin
-client.on('messageCreate', (message) => {
-    if (message.content === '!ping') {
-        message.reply('Pong!');
+    if (oldState.channelId && oldState.channelId !== SOURCE_CHANNEL_ID) {
+        const channel = oldState.channel;
+        if (channel.name.startsWith("Oda -") && channel.members.size === 0) {
+            channel.delete().catch(console.error);
+        }
     }
 });
 
-// TOKEN kısmı asla buraya yazılmaz, Render'daki Environment'tan çeker
-client.login(process.env.TOKEN);
+client.login(TOKEN);
