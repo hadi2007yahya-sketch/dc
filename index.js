59cbd1e489418810476d2deb9e8e5d8d503bb9f8
hadi2007yahya-sketch <hadi2007yahya@gmail.com>
2026-07-08 17:55:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { joinVoiceChannel } = require('@discordjs/voice');
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Bot aktif'));
-app.listen(3000);
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+const client = new Client({ intents: [3276799] });
 
 client.on('ready', () => {
-    console.log(`${client.user.tag} olarak giriş yapıldı!`);
+    console.log(`Giriş yapıldı: ${client.user.tag}`);
+    const channel = client.channels.cache.get("1447158114376880188");
     
-    // Gecikme ekleyelim ki kanal bilgisi cache'e yüklensin
-    setTimeout(() => {
-        const channel = client.channels.cache.get("1447158114376880188");
-        if (channel) {
-            joinVoiceChannel({
-                channelId: channel.id,
-                guildId: channel.guild.id,
-                adapterCreator: channel.guild.voiceAdapterCreator,
-            });
-            console.log("Kanala bağlanıldı.");
-        } else {
-            console.log("Kanal ID bulunamadı, ID'yi kontrol et.");
-        }
-    }, 5000); // 5 saniye bekle
+    if (!channel) {
+        console.log("HATA: Kanalı bulamıyorum! Sunucuda mıyım? ID doğru mu?");
+        return;
+    }
+    
+    try {
+        joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+        });
+        console.log("BAŞARILI: Kanala giriş yaptım.");
+    } catch (e) {
+        console.log("BAĞLANMA HATASI:", e);
+    }
 });
 
 client.login(process.env.TOKEN);
