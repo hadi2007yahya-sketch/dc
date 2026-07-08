ddec1588fe4eefb84d4337ce3c8d8c895eea3e37
hadi2007yahya-sketch <hadi2007yahya@gmail.com>
2026-07-08 17:34:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-const { Client, GatewayIntentBits, ChannelType } = require('discord.js');
-const client = new Client({ 
-    intents: [
-        GatewayIntentBits.Guilds, 
-        GatewayIntentBits.GuildVoiceStates 
-    ] 
-});
-const GUILD_ID = "1446797947399569460";
-const SOURCE_CHANNEL_ID = "1524385072570961920";
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
 
-client.on('voiceStateUpdate', async (oldState, newState) => {
-    if (newState.channelId === SOURCE_CHANNEL_ID) {
-        const guild = newState.guild;
-        const newChannel = await guild.channels.create({
-            name: `Oda - ${newState.member.user.username}`,
-            type: ChannelType.GuildVoice,
-            parent: newState.channel.parent,
+const KANAL_ID = "BURAYA_SES_KANALI_ID_YAZ";
+
+client.once('ready', () => {
+    const channel = client.channels.cache.get(KANAL_ID);
+    if (channel) {
+        joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
         });
-        await newState.member.voice.setChannel(newChannel);
-    }
-
-    if (oldState.channelId && oldState.channelId !== SOURCE_CHANNEL_ID) {
-        const channel = oldState.channel;
-        if (channel.name.startsWith("Oda -") && channel.members.size === 0) {
-            channel.delete().catch(console.error);
-        }
     }
 });
 
-client.login(TOKEN);
+client.login(process.env.TOKEN);
